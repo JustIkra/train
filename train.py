c1e23f1a96a4b4e3828886d637bc7fddfcebbb94
JustIkra <senpaicat2407@gmail.com>
2025-03-13 21:07:51 +0300

    forgotten file

--- train.py
+++ train.py
@@ -5,4 +5,4 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 model = YOLO("yolo11n.pt").to(device)
 
 if __name__ == '__main__':
-    model.train(data = "data_01.yaml", epochs = 100, batch = 0.7, device = 0, plots = True)
+    model.train(data = "data_01.yaml", epochs = 200, batch = 0.7, device = 0, plots = True)
